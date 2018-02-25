9ee8e18507348bc22404966305ff32b9101401a5
LiveG <hi@liveg.tech>
2018-02-25 17:53:16 +0000

    Add copyright and credits to code

--- buildOS.sh
+++ buildOS.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+#   FREEproject OS Builder
+#   The OS tidy-upper and builder for FREEproject OS.
+#
+#   Contributors are shown at https://freeproject-oss.github.io/contributors.html
+#
+#   (C) FREEproject Open Source Systems and contributors 2018.
+
 echo "NOTICE: THIS WILL CLEAR ANY PREFERENCES!"
 echo "Press Enter to continue."
 read enter
