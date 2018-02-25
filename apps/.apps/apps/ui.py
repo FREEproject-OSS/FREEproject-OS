a98a2fcff7419edc485ea6128eee891c42995afb
LiveG-Technologies <liveg2015@hotmail.com>
2018-02-25 12:22:03 +0000

    Add experimental message

---
+++ apps/.apps/apps/ui.py
@@ -0,0 +1,150 @@
+"""
+    FREEproject OS API
+    The API for FREEproject OS.
+
+    Contributors are shown at https://freeproject-oss.github.io/contributors.html
+
+    (C) FREEproject Open Source Systems and contributors 2018.
+"""
+
+import term
+import time
+import textwrap
+
+colours = term.colours
+sensory = []
+
+# Profile Elements	# BG = Background
+					# FG = Foreground
+					# CB = ControlBar
+					# XB = CloseBtton
+probgbg = "white"	# Profile Background BG
+probgfg = "black"	# Profile Background FG
+profgbg = "lblue"	# Profile UI Element BG
+profgfg = "white"	# Profile UI Element FG
+proscbg = "blue"	# Profile Top Screen BG
+prosccb = "lblue"	# Profile Top Screen CB
+proscxb = "lred"	# Profile Top Screen XB
+proscfg = "white"	# Profile Top Screen FG
+promgbg = "lgrey"	# Profile UI Message BG
+promgfg = "black"	# Profile UI Message FG
+
+def screen(title, closeButton = True, clear = True):
+	if clear: term.clear(bg = probgbg, fg = probgfg)
+	
+	term.goto(0, 0)
+	term.echo(term.colour(" " * 79, bg = proscbg, fg = proscfg))
+	term.goto(0, 0)
+	term.echo(term.colour(" " * 5, bg = prosccb, fg = proscfg))
+	term.goto(0, 0)
+	term.echo(term.colour(time.strftime("%H:%M", time.gmtime()), bg = prosccb, fg = proscfg))
+	term.goto(6, 0)
+	term.echo(term.colour(title, bg = proscbg, fg = proscfg))
+	
+	if closeButton:
+		term.goto(80, 0)
+		term.echo(term.colour("X", bg = proscxb, fg = proscfg))
+	
+def message(text, title = "Information", mode = "OK"):
+	text = text[:580]
+	
+	wrapper = textwrap.TextWrapper(break_long_words = True, break_on_hyphens = True)
+	wrapper.width = 56
+	strings = wrapper.wrap(text)
+	messageWidth = 0
+	
+	title = title[:60]
+	
+	for line in range(0, len(strings)):
+		if len(strings[line]) > messageWidth: messageWidth = len(strings[line]) + 2
+	
+	if len(title) > messageWidth: messageWidth = len(title)
+		
+	if mode == "OK":
+		if messageWidth < 6: messageWidth = 6
+	elif mode == "OK+Cancel":
+		if messageWidth < 15: messageWidth = 15
+	elif mode == "Yes+No" or mode == "No+Yes":
+		if messageWidth < 12: messageWidth = 12
+	
+	term.goto(5, 5)
+	term.echo(term.colour(" " * messageWidth, bg = proscbg, fg = proscfg))
+	term.goto(5, 5)
+	term.echo(term.colour(title, bg = proscbg, fg = proscfg))
+	term.goto(5, 6)
+	term.echo(term.colour(" " * messageWidth, bg = promgbg, fg = promgfg))
+	
+	for line in range(0, len(strings)):
+		term.goto(5, 7 + line)
+		term.echo(term.colour(" " * messageWidth, bg = promgbg, fg = promgfg))
+		term.goto(6, 7 + line)
+		term.echo(term.colour(strings[line], bg = promgbg, fg = promgfg))
+		
+	term.goto(5, 7 + len(strings))
+	term.echo(term.colour(" " * messageWidth, bg = promgbg, fg = promgfg))
+	term.goto(5, 8 + len(strings))
+	term.echo(term.colour(" " * messageWidth, bg = promgbg, fg = promgfg))
+	term.goto(5, 9 + len(strings))
+	term.echo(term.colour(" " * messageWidth, bg = promgbg, fg = promgfg))
+	term.goto(5, 10 + len(strings))
+	term.echo(term.colour(" " * messageWidth, bg = promgbg, fg = promgfg))
+	term.goto(5, 11 + len(strings))
+	term.echo(term.colour(" " * messageWidth, bg = promgbg, fg = promgfg))
+	
+	if mode == "OK":
+		button("OK", messageWidth, 8 + len(strings), big = True)
+	elif mode == "OK+Cancel":
+		button("OK", messageWidth, 8 + len(strings), big = True)
+		button("Cancel", messageWidth - 9, 8 + len(strings), big = True, bg = proscxb)
+	elif mode == "Yes+No":
+		button("Yes", messageWidth - 1, 8 + len(strings), big = True)
+		button("No", messageWidth - 6, 8 + len(strings), big = True, bg = proscxb)
+	elif mode == "No+Yes":
+		button("No", messageWidth, 8 + len(strings), big = True)
+		button("Yes", messageWidth - 6, 8 + len(strings), big = True, bg = proscxb)
+		
+	senseBtn = -1
+	while senseBtn == -1 or senseBtn == -2:
+		senseBtn = sense(clear = False)
+	
+	clearSense()
+	return senseBtn
+	
+def button(text, x, y, big = False, bg = profgbg, fg = profgfg):
+	global sensory
+	
+	if big:
+		term.goto(x, y)
+		term.echo(term.colour(" " * (len(text) + 2), bg = bg, fg = fg))
+		term.goto(x, y + 1)
+		term.echo(term.colour(" " + text + " ", bg = bg, fg = fg))
+		term.goto(x, y + 2)
+		term.echo(term.colour(" " * (len(text) + 2), bg = bg, fg = fg))
+		sensory.append([x, y, x + len(text) + 2, y + 2])
+	else:
+		term.goto(x, y)
+		term.echo(term.colour(text, bg = bg, fg = fg))
+		sensory.append([x, y, x + len(text), y])
+		
+def text(text, x, y, bg = probgbg, fg = probgfg):
+	term.goto(x, y)
+	term.echo(term.colour(text, bg = bg, fg = fg))
+	
+def sense(clear = True, screen = True):
+	global sensory
+	mousepos = term.mouse()
+	senseitem = -1
+	
+	for item in range(0, len(sensory)):
+		if mousepos[0] >= sensory[item][0] and mousepos[0] <= sensory[item][2] and mousepos[1] >= sensory[item][1] and mousepos[1] <= sensory[item][3]: senseitem = item;
+	
+	if screen:
+		if mousepos[0] == 79 and mousepos[1] == 0:
+			senseitem = -2
+	
+	if clear: sensory = []
+	return senseitem
+	
+def clearSense(): global sensory; sensory = []
+
+term.init()
