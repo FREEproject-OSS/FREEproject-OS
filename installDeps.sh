b804c9b34c974d8eff5f483ef70cf42ca721575c
LiveG-Technologies <liveg2015@hotmail.com>
2018-02-25 17:49:30 +0000

    Create dependency installer

---
+++ installDeps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#   FREEproject OS Dependency Installer
+#   The update dependency installer for FREEproject OS.
+#
+#   Contributors are shown at https://freeproject-oss.github.io/contributors.html
+#
+#   (C) FREEproject Open Source Systems and contributors 2018.
+
+# To install apt-get packages, insert:
+# sudo apt-get install package-name
+
+# To install pip3 libraries, insert:
+# sudo pip3 install library-name
