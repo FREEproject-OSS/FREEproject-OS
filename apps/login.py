4c7a24890f7b4c2154d4148102c95b2fc9d8c71e
LiveG-Technologies <liveg2015@hotmail.com>
2018-03-03 16:28:54 +0000

    Create screen in login

--- apps/login.py
+++ apps/login.py
@@ -7,8 +7,42 @@
     (C) FREEproject Open Source Systems and contributors 2018.
 """
 
+import term
 import ui
 
-ui.button("Login", 15, 15)
-item = ui.sense()
-print(item)
+button = -1
+action = 0
+
+while action == 0:
+	while button == -1:
+		term.clear(bg = ui.prolibg, fg = ui.prolifg)
+		
+		term.goto(5, 5); term.echo( "   " + term.colour("          ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 6); term.echo( " " + term.colour("   ***        ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 7); term.echo( " " + term.colour("   ***  ***   ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 8); term.echo(      term.colour("    *    *  *   ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 9); term.echo(      term.colour("    ***  *  *   ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 10); term.echo(     term.colour("    ***  ***    ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 11); term.echo(" " + term.colour("   *    *     ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 12); term.echo(" " + term.colour("        *     ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		term.goto(5, 13); term.echo("   " + term.colour("          ", bg = "lblue").replace("*", term.colour(" ", bg = "black") + term.colour("", bg = "lblue", revert = False)))
+		
+		term.goto(23, 6); term.echo( "*** **  *** *** **   **  *   *  *   ** *     *   **".replace("*", term.colour(" ", bg = "white")))
+		term.goto(23, 7); term.echo( "*** *** *** *** * * *   * *    * * *   *    * * *".replace("*", term.colour(" ", bg = "white")))
+		term.goto(23, 8); term.echo( "*   * * *   *   * * *   * *  * *** *   **   * * **".replace("*", term.colour(" ", bg = "white")))
+		term.goto(23, 9); term.echo( "*** **  *** *** **  *   * *  * *   *   *    * *   *".replace("*", term.colour(" ", bg = "white")))
+		term.goto(23, 10); term.echo("*** **  *   *   *   *   * *  * *   *   *    * *   *".replace("*", term.colour(" ", bg = "white")))
+		term.goto(23, 11); term.echo("*   * * *** *** *   *    *  *   **  **  *    *  **".replace("*", term.colour(" ", bg = "white")))
+		
+		ui.button("Login", x = 36, y = 15, big = True)
+		ui.button("Power", x = 70, y = 28, big = False, bg = ui.proscxb, fg = ui.proscfg)
+		button = ui.sense(screen = False)
+		
+	if button == 0:
+		action = 1
+	elif button == 1:
+		ui.button("Shutdown", x = 67, y = 26, big = False, bg = ui.prodmbg, fg = ui.prodmfg)
+		ui.button("Restart ", x = 67, y = 27, big = False, bg = ui.prodmbg, fg = ui.prodmfg)
+		
+		dmaction = ui.sense(screen = False)
+		button = -1
