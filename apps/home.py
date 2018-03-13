8210351c91c5dfb4cb0cd0e32ab436fefa9023e8
LiveG-Technologies <liveg2015@hotmail.com>
2018-03-13 13:00:47 +0000

    Add contributors and copyright to code

--- apps/home.py
+++ apps/home.py
@@ -1,1 +1,13 @@
+"""
+    FREEproject OS Home
+    The post-login home screen for FREEproject OS.
+
+    Contributors are shown at https://freeproject-oss.github.io/contributors.html
+
+    (C) FREEproject Open Source Systems and contributors 2018.
+"""
+
+import term
+import ui
+
 # TODO: Create a home screen.
